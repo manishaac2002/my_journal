0653c072115e5c5fbe1bf64477653a4e022fdb67
manishaac2002 <manishaac2002@gmail.com>
2024-12-19 16:51:26 +0530

    added section for list view.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,13 @@ function App() {
       <div
         className="mt-10 ml-5 mr-4 ">
         <h1 className='text-3xl font-bold mb-4'>Personal Journal</h1>
+        <div className="grid grid-cols-2">
+          <div className=" mr-10 box-border border-2 border-gray-300 rounded">
+            <p className="text-2xl font-semibold text-gray-600 px-5 py-4">Your Journal List </p>
+          </div>
+         
         <Jodit />
+        </div>
       </div>
       
       <button className='py-3 px-3 mt-4 ml-3 rounded bg-blue-600 text-white' onClick={clickEvent}>
